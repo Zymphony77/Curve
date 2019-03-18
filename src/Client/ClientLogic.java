bcb21ca4fd838faa7b2c6c31c60f9a33276bc252
Chalat Limpongsawatt <chalat.lim@gmail.com>
2019-03-18 21:30:13 +0700

    Update Client logic that only have function's name & parameter

--- src/Client/ClientLogic.java
+++ src/Client/ClientLogic.java
@@ -1,69 +1,55 @@
 package Client;
 
-import java.util.Vector;
+import utility.csv.*;
 
 public class ClientLogic {
-	
-//	Create Client
-	public static void CreateClientEvent (String Username) {
-		
-		
-	}
-	public static void NewClientEvent () {
 
-		
-	}
-	
-//	Connecting	
-	public static void ConnectEvent (int cid, String Username) {
+	// Create Client
+	public static void CreateClientEvent(String Username) {
 
-		
 	}
-	
-	public static void DisconnectEvent (int cid, String Username) {
 
-	
-	}
-	
-	
-//	Group Event
-	public static void CreateGroupEvent (int cid, String Groupname) {
+	public static void NewClientEvent(Object X) {
 
-	
 	}
-	
-	public static void JoinEvent (String Groupname) {
 
-	
-	}
-	
-	public static void LeaveEvent (int gid) {
+	// Connecting
+	public static void ConnectEvent(int cid, String Username) {
 
-	
 	}
-	
-	public static void UpdateGroupEvent (int gid) {
 
-		
-	}
-	
-//	Message Event
-	public static void GetUnreadEvent (int gid) {
+	public static void DisconnectEvent(int cid, String Username) {
 
-		
 	}
-	
-	public static void SendEvent (String Message, int cid, int gid) {
 
-		
-	}
-	
-	public static void UpdateMessageEvent (int cid, int gid) {
+	// Group Event
+	public static void CreateGroupEvent(int cid, String Groupname) {
 
-	
 	}
-	
-	
-	
-	
+
+	public static void JoinEvent(String Groupname) {
+
+	}
+
+	public static void LeaveEvent(int gid) {
+
+	}
+
+	public static void UpdateGroupEvent(Object X) {
+
+	}
+
+	// Message Event
+	public static void GetUnreadEvent(int gid) {
+
+	}
+
+	public static void SendEvent(String Message, int cid, int gid) {
+
+	}
+
+	public static void UpdateMessageEvent(Object X) {
+
+	}
+
 }
