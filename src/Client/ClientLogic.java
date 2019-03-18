776cea6d021154d1c465eed26fa019c21a173b40
Chalat Limpongsawatt <chalat.lim@gmail.com>
2019-03-18 21:09:28 +0700

    Create client logic

---
+++ src/Client/ClientLogic.java
@@ -0,0 +1,69 @@
+package Client;
+
+import java.util.Vector;
+
+public class ClientLogic {
+	
+//	Create Client
+	public static void CreateClientEvent (String Username) {
+		
+		
+	}
+	public static void NewClientEvent () {
+
+		
+	}
+	
+//	Connecting	
+	public static void ConnectEvent (int cid, String Username) {
+
+		
+	}
+	
+	public static void DisconnectEvent (int cid, String Username) {
+
+	
+	}
+	
+	
+//	Group Event
+	public static void CreateGroupEvent (int cid, String Groupname) {
+
+	
+	}
+	
+	public static void JoinEvent (String Groupname) {
+
+	
+	}
+	
+	public static void LeaveEvent (int gid) {
+
+	
+	}
+	
+	public static void UpdateGroupEvent (int gid) {
+
+		
+	}
+	
+//	Message Event
+	public static void GetUnreadEvent (int gid) {
+
+		
+	}
+	
+	public static void SendEvent (String Message, int cid, int gid) {
+
+		
+	}
+	
+	public static void UpdateMessageEvent (int cid, int gid) {
+
+	
+	}
+	
+	
+	
+	
+}
