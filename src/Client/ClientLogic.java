a1fa65fd71d71332e2bab86dd84c3fc7fba64aad
Chalat Limpongsawatt <chalat.lim@gmail.com>
2019-03-19 10:04:53 +0700

    Update Client logic method

--- src/Client/ClientLogic.java
+++ src/Client/ClientLogic.java
@@ -1,53 +1,65 @@
 package Client;
 
+import java.sql.Timestamp;
 import utility.csv.*;
-import utility.event.*;
+import utility.event.ConnectEvent;
+import utility.event.CreateClientEvent;
+import utility.event.CreateGroupEvent;
+import utility.event.DisconnectEvent;
+import utility.event.GetUnreadMessageEvent;
+import utility.event.JoinGroupEvent;
+import utility.event.LeaveGroupEvent;
+import utility.event.NewClientEvent;
+import utility.event.NewGroupEvent;
+import utility.event.NewMessageEvent;
+import utility.event.SendMessageEvent;
+
 public class ClientLogic {
 
 	// Create Client
-	public static void CreateClient(String Username) {
-
+	public static void CreateClient(String clientName) {
+		CreateClientEvent createClient = new CreateClientEvent(clientName);
 	}
 
-	public static void NewClient(Object X) {
+	public static void NewClient(NewClientEvent newClient) {
 
 	}
 
 	// Connecting
 	public static void Connect(int cid, String ipAddress) {
-		ConnectEvent connectEvent = new ConnectEvent(cid,ipAddress);
-		
+		ConnectEvent connect = new ConnectEvent(cid, ipAddress);
+
 	}
 
 	public static void Disconnect(int cid) {
-		DisconnectEvent disconnectEvent = new DisconnectEvent(cid);
-		
+		DisconnectEvent disconnect = new DisconnectEvent(cid);
+
 	}
 
 	// Group Event
-	public static void CreateGroup(int cid, String Groupname) {
-
+	public static void CreateGroup(int cid, String groupName) {
+		CreateGroupEvent createGroup = new CreateGroupEvent(cid, groupName);
 	}
 
-	public static void Join(String Groupname) {
-
+	public static void Join(int cid, int gid) {
+		JoinGroupEvent joinGroup = new JoinGroupEvent(cid, gid);
 	}
 
-	public static void Leave(int gid) {
-
+	public static void Leave(int cid, int gid) {
+		LeaveGroupEvent leaveGroup = new LeaveGroupEvent(cid, gid);
 	}
 
 	public static void NewGroup(NewGroupEvent newGroup) {
-			
+
 	}
 
 	// Message Event
-	public static void GetUnread(int gid) {
-
+	public static void GetUnreadMesaage(int cid, Timestamp lastestTimestamp) {
+		GetUnreadMessageEvent getUnreadMessage = new GetUnreadMessageEvent(cid, lastestTimestamp);
 	}
 
-	public static void Send(String Message, int cid, int gid) {
-
+	public static void SendMessage(int cid, int gid, String Message) {
+		SendMessageEvent sendMessageEvent = new SendMessageEvent(cid, gid, Message);
 	}
 
 	public static void NewMessage(NewMessageEvent newMessage) {
